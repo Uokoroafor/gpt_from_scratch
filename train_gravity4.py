0f49990798e5037ad15476880f53b73caa5aada9
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-07-17 09:55:53 +0100

    Trying a minimal dataset 3

--- train_gravity4.py
+++ train_gravity4.py
@@ -181,6 +181,8 @@ def train(model, data_loader, loss_fn, optimizer, device):
         optimizer.zero_grad()
 
         outputs = model(inputs)
+        print('outputs: ', outputs.shape)
+        print('targets: ', targets.shape)
         loss = loss_fn(outputs.view(-1, outputs.size(-1)), targets.view(-1))
 
         loss.backward()
