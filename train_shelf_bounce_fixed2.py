2c1d1427b336463e9ed0da006e071801304af480
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-08-10 22:14:23 +0100

    Testing more flexible Architecture 6

--- train_shelf_bounce_fixed2.py
+++ train_shelf_bounce_fixed2.py
@@ -37,102 +37,108 @@ test_data_path = f"test_{function_name}.csv"
 for output_type in ["text", "num"]:
     print(f'Training for output type: {output_type}')
     # output_type = "text"  # 'num' or 'text'
+    for use_bpe in [True, False]:
+        if use_bpe:
+            print("Using BPE")
+        else:
+            print("Using char-level encoding")
+        # use_bpe = False  # Set to True to use BPE, False to use a character encoder/decoder
 
-    use_bpe = False  # Set to True to use BPE, False to use a character encoder/decoder
+        # Read in the data
+        data = read_in_data(data_folder + file_path, make_dict=False)
 
-    # Read in the data
-    data = read_in_data(data_folder + file_path, make_dict=False)
+        # Create the tokeniser
+        if use_bpe:
+            bpe = BPE(data)
+            # Train for 50 iterations
+            bpe.train(50)
+            gpt_tokeniser = bpe
+        else:
+            # Use BasicTokeniser for char-level encoding
+            gpt_tokeniser = BasicTokeniser(data)
 
-    # Create the tokeniser
-    if use_bpe:
-        bpe = BPE(data)
-        # Train for 50 iterations
-        bpe.train(50)
-        gpt_tokeniser = bpe
-    else:
-        # Use BasicTokeniser for char-level encoding
-        gpt_tokeniser = BasicTokeniser(data)
+        # Create the encoder and decoder dictionaries and the encode and decode functions
+        encoder_dict, decoder_dict, encode, decode = (
+            gpt_tokeniser.lookup_table,
+            gpt_tokeniser.reverse_lookup_table,
+            gpt_tokeniser.encode,
+            gpt_tokeniser.decode,
+        )
 
-    # Create the encoder and decoder dictionaries and the encode and decode functions
-    encoder_dict, decoder_dict, encode, decode = (
-        gpt_tokeniser.lookup_table,
-        gpt_tokeniser.reverse_lookup_table,
-        gpt_tokeniser.encode,
-        gpt_tokeniser.decode,
-    )
+        encoding_utils = dict(
+            enc_dict=encoder_dict, dec_dict=decoder_dict, encode_fn=encode, decode_fn=decode
+        )
 
-    encoding_utils = dict(
-        enc_dict=encoder_dict, dec_dict=decoder_dict, encode_fn=encode, decode_fn=decode
-    )
+        # Read in the data as pandas dataframes
+        train_data = pd.read_csv(data_folder + train_data_path, dtype=str)
+        val_data = pd.read_csv(data_folder + val_data_path, dtype=str)
+        test_data = pd.read_csv(data_folder + test_data_path, dtype=str)
 
-    # Read in the data as pandas dataframes
-    train_data = pd.read_csv(data_folder + train_data_path, dtype=str)
-    val_data = pd.read_csv(data_folder + val_data_path, dtype=str)
-    test_data = pd.read_csv(data_folder + test_data_path, dtype=str)
+        train_loader, val_loader, test_loader, max_seq_len = make_data_loaders(
+            tokeniser=gpt_tokeniser,
+            train_data=train_data,
+            val_data=val_data,
+            test_data=test_data,
+            batch_size=batch_size,
+            output=output_type,
+            shuffle=True,
+        )
 
-    train_loader, val_loader, test_loader, max_seq_len = make_data_loaders(
-        tokeniser=gpt_tokeniser,
-        train_data=train_data,
-        val_data=val_data,
-        test_data=test_data,
-        batch_size=batch_size,
-        output=output_type,
-        shuffle=True,
-    )
+        # update block size to be the max sequence length
+        block_size = max_seq_len
 
-    # update block size to be the max sequence length
-    block_size = max_seq_len
+        # Create the model, loss function and optimiser
+        loss_fn = (
+            nn.MSELoss()
+            if output_type == "num"
+            else nn.CrossEntropyLoss(ignore_index=encoder_dict[gpt_tokeniser.pad])
+        )
 
-    # Create the model, loss function and optimiser
-    loss_fn = (
-        nn.MSELoss()
-        if output_type == "num"
-        else nn.CrossEntropyLoss(ignore_index=encoder_dict[gpt_tokeniser.pad])
-    )
+        model = DecodeOnlyTransformer(
+            src_pad=encoder_dict["<pad>"],
+            src_sos=encoder_dict["<sos>"],
+            vocab_size_enc=len(encoder_dict),
+            output_size=1 if output_type == "num" else len(encoder_dict),
+            pooling="max" if output_type == "num" else "none",
+            max_seq_len=block_size,
+            num_heads=training_hyperparams["num_heads"],
+            num_layers=training_hyperparams["num_layers"],
+            d_model=training_hyperparams["d_model"],
+            d_ff=training_hyperparams["d_ff"],
+            dropout_prob=training_hyperparams["dropout_prob"],
+            device=device,
+        )
 
-    model = DecodeOnlyTransformer(
-        src_pad=encoder_dict["<pad>"],
-        src_sos=encoder_dict["<sos>"],
-        vocab_size_enc=len(encoder_dict),
-        output_size=1 if output_type == "num" else len(encoder_dict),
-        pooling="max" if output_type == "num" else "none",
-        max_seq_len=block_size,
-        num_heads=training_hyperparams["num_heads"],
-        num_layers=training_hyperparams["num_layers"],
-        d_model=training_hyperparams["d_model"],
-        d_ff=training_hyperparams["d_ff"],
-        dropout_prob=training_hyperparams["dropout_prob"],
-        device=device,
-    )
+        optimiser = torch.optim.Adam(model.parameters(), lr=lr)
+        scheduler = None
 
-    optimiser = torch.optim.Adam(model.parameters(), lr=lr)
-    scheduler = None
+        device = torch.device(training_hyperparams["device"])
 
-    device = torch.device(training_hyperparams["device"])
+        # Move the model and loss function to the device
+        model = model.to(device)
+        loss_fn = loss_fn.to(device)
 
-    # Move the model and loss function to the device
-    model = model.to(device)
-    loss_fn = loss_fn.to(device)
+        trainer = PhysicalTrainer(
+            model=model,
+            optimiser=optimiser,
+            loss_fn=loss_fn,
+            training_hyperparameters=training_hyperparams,
+            encoding_utils=encoding_utils,
+            scheduler=scheduler,
+        )
 
-    trainer = PhysicalTrainer(
-        model=model,
-        optimiser=optimiser,
-        loss_fn=loss_fn,
-        training_hyperparameters=training_hyperparams,
-        encoding_utils=encoding_utils,
-        scheduler=scheduler,
-    )
+        model, _, _ = trainer.train(
+            train_dataloader=train_loader,
+            val_dataloader=val_loader,
+            save_model=True,
+            plotting=True,
+            verbose=True,
+            early_stopping=True,
+            early_stopping_patience=20,
+        )
 
-    model, _, _ = trainer.train(
-        train_dataloader=train_loader,
-        val_dataloader=val_loader,
-        save_model=True,
-        plotting=True,
-        verbose=True,
-        early_stopping=True,
-        early_stopping_patience=10,
-    )
+        encoding_string = "bpe" if use_bpe else "char"
 
-    trainer.log_numerical_outputs(
-        test_loader, decode, f"{output_type}_test_log.txt", output_type=output_type
-    )
+        trainer.log_numerical_outputs(
+            test_loader, decode, f"{output_type}_test_log_{encoding_string}.txt", output_type=output_type
+        )
